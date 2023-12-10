fbd1d93b48cf61f62be9e53bd6e6b35244948d5b
Alexander Lovchikov <sanusallow@gmail.com>
2023-12-10 17:03:31 +0300

    Add working regular expression for datehandler

--- internal/controller/datehandler.go
+++ internal/controller/datehandler.go
@@ -12,15 +12,15 @@ import (
 
 func check(s string) bool {
 	// 99.99.9999 99:99
-	return regexp.MustCompile(`\d\d.\d\d.\d\d\d\d \d\d:\d\d`).MatchString(s)
+	return regexp.MustCompile(`(0[1-9]|1[0-2])\.(0[1-9]|[12][0-9]|3[01])\.(20[0-9]{2}|21[0-1][0-9])\s(0[0-9]|1[0-9]|2[0-3]):([0-5][0-9])`).MatchString(s)
 }
 
 func (c *Controller) DateHandler(update tgbotapi.Update) error {
 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, languages.MessageEnterText)
 
-	//	if check(update.Message.Text) {
-	//		c.alertDB.AddAlert()
-	//	}
+	if check(update.Message.Text) {
+		c.alertDB.AddAlert(db.Alert{})
+	}
 
 	err := c.userDB.UpdateState(strconv.FormatInt(update.Message.Chat.ID, 10), db.CreateTextState)
 	if err != nil {
